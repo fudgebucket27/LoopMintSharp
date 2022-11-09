927088f7f254dd8e82e81b4be719eefc7d2dc713
Fudgey <fudgebucket27@gmail.com>
2022-11-09 13:56:42 +1000

    fix csv date

--- LoopMintSharp/Program.cs
+++ LoopMintSharp/Program.cs
@@ -156,7 +156,7 @@ else if (args[0].Trim() == "-legacymintcollection" )
         }
     }
 
-    string csvName = $"{DateTime.Now.ToString("yyyy-mm-dd hh-mm-ss")}.csv";
+    string csvName = $"{DateTime.Now.ToString("yyyy-MM-dd hh-mm-ss")}.csv";
     using (var writer = new StreamWriter(csvName))
     using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
     {
@@ -254,7 +254,7 @@ else if (args[0].Trim() == "-mintcollection")
         }
     }
 
-    string csvName = $"{DateTime.Now.ToString("yyyy-mm-dd hh-mm-ss")}.csv";
+    string csvName = $"{DateTime.Now.ToString("yyyy-MM-dd hh-mm-ss")}.csv";
     using (var writer = new StreamWriter(csvName))
     using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
     {
