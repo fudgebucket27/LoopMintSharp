98ccf095f79ee34125d4820bd35cfb40db7ed09d
Fudgey <fudgebucket27@gmail.com>
2023-04-21 13:10:04 +1000

    removed unused method

--- LoopMintSharp/Helpers/Utils.cs
+++ LoopMintSharp/Helpers/Utils.cs
@@ -30,19 +30,5 @@ namespace LoopMintSharp
             stringToEncode = HttpUtility.UrlEncode(stringToEncode);
             return reg.Replace(stringToEncode, m => m.Value.ToUpperInvariant());
         }
-        public static IEnumerable<T> FromDelimitedJson<T>(TextReader reader, JsonSerializerSettings settings = null)
-        {
-            using (var jsonReader = new JsonTextReader(reader) { CloseInput = false, SupportMultipleContent = true })
-            {
-                var serializer = JsonSerializer.CreateDefault(settings);
-
-                while (jsonReader.Read())
-                {
-                    if (jsonReader.TokenType == JsonToken.Comment)
-                        continue;
-                    yield return serializer.Deserialize<T>(jsonReader);
-                }
-            }
-        }
     }
 }
